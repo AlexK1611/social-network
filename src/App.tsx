52c97e435bd0b35916dc13e44b8084821a5fc139
AlexK1611 <iltr1611@gmail.com>
2022-03-01 18:16:50 +0200

    app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom'
 import { AppLayout } from 'layouts/AppLayout/AppLayout'
 
 const App = () => {
   return (
-    <AppLayout>
-      
-    </AppLayout>
+    <BrowserRouter>
+      <AppLayout>
+        <Routes>
+          <Route path='/' element={<p>main</p>} />
+          <Route path='/friends' element={<p>friends</p>} />
+          <Route path='/albums' element={<p>albums</p>} />
+        </Routes>
+      </AppLayout>
+    </BrowserRouter>
   )
 }
 
