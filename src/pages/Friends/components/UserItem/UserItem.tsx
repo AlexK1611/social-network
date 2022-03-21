d7dacb4b56ce4fb5f71c07048a68dd737e13dd24
AlexK1611 <iltr1611@gmail.com>
2022-03-21 16:41:11 +0200

    friends page recolored

--- src/pages/Friends/components/UserItem/UserItem.tsx
+++ src/pages/Friends/components/UserItem/UserItem.tsx
@@ -1,11 +1,14 @@
 import { FC } from 'react'
 import styles from './UserItem.module.scss'
 import { IUserItemProps } from './UserItem.types'
+import { ReactComponent as UserIcon } from 'assets/icons/user.svg'
 
 export const UserItem: FC<IUserItemProps> = ({ username }) => {
     return (
         <div className={styles.userItem}>
-            <div className={styles.userAvatar}></div>
+            <div className={styles.userAvatarContainer}>
+                <UserIcon className={styles.userAvatar} />
+            </div>
             <p className={styles.userNickname}>
                 {username}
             </p>
